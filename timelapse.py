a7c86d32de811e137f9b2c674ca7bcad328aadf0
Grant Francis <gfrancis@uvic.ca>
2021-06-14 16:07:48 -0700

    timelapse for cumulative & diff changes

--- timelapse.py
+++ timelapse.py
@@ -14,20 +14,19 @@ import numpy as np
 
 
 
-lib = r'C:\Users\gfrancis\Documents\Planet\WR_timeline\timelapse'
+lib = r'C:\Users\gfrancis\Documents\Planet\WR_timeline\Priority_Regions\differences'
 
 
 i = 0
-for pic in glob.glob(lib + '\\*.tif'):
+for pic in glob.glob(lib + '\\*.jpg'):
     G = Image.open(pic)
     arr_G = np.array(G)
-    Image.fromarray(arr_G.astype(np.uint8)).save(lib + '\\%04d.jpg'%i)
+    Image.fromarray(arr_G.astype(np.uint8)).convert('RGB').save(lib + '\\frames\\%04d.jpg'%i)
     i += 1
 
 
 
-
-os.chdir(lib)
+os.chdir(lib + '\\frames')
 os.system('ffmpeg -r 16 -f image2 -i %4d.jpg -s 4608x3456 -c:v mjpeg -q:v 10 ./timelapse.mov')
 
 
