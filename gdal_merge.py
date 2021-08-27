aa26a71e0516c255e7668bcb7e2b1cb857489c18
Grant <gfrancis@uvic.ca>
2021-08-27 23:09:17 +0000

    banks timeline setup feynman

--- gdal_merge.py
+++ gdal_merge.py
@@ -2,8 +2,8 @@
 
 import sys
 # import osgeo.utils.gdal_merge as a convenience to use as a script
-from osgeo.utils.gdal_merge import *  # noqa
-from osgeo.utils.gdal_merge import main
+from osgeo_utils.gdal_merge import *  # noqa
+from osgeo_utils.gdal_merge import main
 from osgeo.gdal import deprecation_warn
 
 
--- merge_multi.py
+++ merge_multi.py
@@ -10,28 +10,30 @@ Created on Wed Jun  9 11:43:23 2021
 import os
 ### Set OSGEO env PATHS
 home = os.path.expanduser('~')
-os.environ['PROJ_LIB'] = home + r'\Appdata\Roaming\Python\Python37\site-packages\osgeo\data\proj'
-os.environ['GDAL_DATA'] = home + r'\Appdata\Roaming\Python\Python37\site-packages\osgeo\data'
+os.environ['PROJ_LIB'] = '/usr/share/proj'
+os.environ['GDAL_DATA'] = '/usr/share/gdal'
 import glob
+import osgeo_utils
 
 
-gdal_merge = r'C:\Users\gfrancis\Documents\Code\tools\gdal_merge.py'
-top_folder = 'C:/Users/gfrancis/Documents/Planet/WR_timline/**/files'
-out_dir = r'C:\Users\gfrancis\Documents\Planet\WR_timline\mosaics'
+gdal_merge = home + '/repos/tools/gdal_merge.py'
+top_folder = home + '/Planet/Banks_timeline/downloads/**/files'
+out_dir = home + '/Planet/Banks_timeline/mosaics'
 
 
 
 for directory in glob.glob(top_folder):
     
-    name = directory[46:62]
-    out_file = out_dir + '\\' + name + '.tif'
+    name = directory[51:59]
+    # print(name)
+    out_file = out_dir + '/' + name + '.tif'
     os.system('echo merging: %s'%name)
     
     pieces = ''
     for piece in glob.glob(directory + '/*SR_clip.tif'):
         pieces += ' ' + piece
     
-    os.system(gdal_merge + ' -o ' + out_file + pieces)
+    os.system('sudo python ' + gdal_merge + ' -o ' + out_file + pieces)
     
     
 
