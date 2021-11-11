2f6d97f5b87ed5ee308eb3fdd1e2b4442e09fd42
Grant <gfrancis@uvic.ca>
2021-11-11 02:15:09 +0000

    WR focus update

--- renamer.py
+++ renamer.py
@@ -15,39 +15,37 @@ from natsort import natsorted
 
 
 
-
-
 def get_name(file_location):
     filename = file_location.split('/')[-1]
     filename = filename.split('.')
-    return filename[0]
+    return filename[0], filename[1]
 
 
-lib = '/home/feynman/Planet/HWC/TL1_HWC/masks'
+lib = '/home/feynman/Planet/WR_Timeline/WR_Timeline_Maps/'
 
 
-# for file in glob.glob(file_dir + '/*.tif'):
+for file in natsorted(glob.glob(lib + '/WR*')):
     
-#     fn = get_name(file)
-#     fn = fn.replace('(', '')
-#     fn = fn.replace(')', '')
-#     os.rename(file, file_dir + '/%s.tif'%fn)
+    fn, ext = get_name(file)
+    fn = fn.replace('WR', '')
+    # fn = fn.replace('WR', '')
+    # print(fn, ' ', ext)
+    os.rename(file, lib + '/%s.%s'%(fn,ext))
 
 
 
-
-print('Re-numbering...')
-count = 0
-for pic in natsorted(glob.glob(lib + '/*.png')):
-    num = int(get_name(pic))
-    num += 107
-    os.rename(pic, lib + '/%s.png'%num)
-    # count += 1
-    # if count > 10: break
-count = 0
-# for pic in glob.glob(lib + '/*.tif'):
-    # os.rename(pic, lib + '/%s.tif'%count)
-    # count += 1
+# print('Re-numbering...')
+# count = 0
+# for pic in natsorted(glob.glob(lib + '/*.png')):
+#     num = int(get_name(pic))
+#     num += 107
+#     os.rename(pic, lib + '/%s.png'%num)
+#     # count += 1
+#     # if count > 10: break
+# count = 0
+# # for pic in glob.glob(lib + '/*.tif'):
+#     # os.rename(pic, lib + '/%s.tif'%count)
+#     # count += 1
 
 
 
